efcbec39be09ecdfb88accf722e5f9772593a95e
josuerushanika <josuerushanika@gmail.com>
2023-04-21 12:48:06 +0200

    Add function for main and enumerable

---
+++ main.rb
@@ -0,0 +1,14 @@
+require_relative "enumerable"
+
+class MyList
+    include  MyEnumerable
+    def initialize(*list)
+      @list = list
+    end
+
+    def each(&block)
+        @list.each(&block)
+    end
+end
+ list = MyList.new(1, 2, 3, 4)
+ puts list.all? {|e| e < 5}
